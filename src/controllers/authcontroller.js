7ec3423b1b96cf4b390c623276471f9372558c97
skale0181 <skale0181@gmail.com>
2022-04-12 21:50:40 +0530

    done

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const newToken = (user) => {
-  return jwt.sign({ user }, process.env.JWT_SECRET_KEY);
+  return jwt.sign({ user }, " "+process.env.JWT_SECRET_KEY);
 };
 
 const register = async (req, res) => {
